b7cd8581962a4214968ef8a4211c8b7f72b75f1d
Ram Kandasamy <ram@code.org>
2017-03-28 18:28:17 -0700

    Remove unused model method

--- dashboard/app/models/experiment.rb
+++ dashboard/app/models/experiment.rb
@@ -21,13 +21,6 @@ class Experiment < ActiveRecord::Base
   belongs_to :section
   validate :set_for_either_user_or_section
 
-  def self.active_experiments(user, script = nil)
-    section = user.sections_as_student.where(script: script) if script
-    experiments = where(user: user)
-    experiments = experiments.or(where(section: section)) if section
-    experiments.where('expiration > ?', DateTime.now)
-  end
-
   def set_for_either_user_or_section
     unless section_id || user_id
       errors.add(:base, message: "experiment must be set for a user or section")
